1f9cefbce1ed4b4757d93f23f483d005e312491c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2023-04-20 11:14:31 +0200

    Use .toSeq instead of .deep for comparison

--- backend/src/test/scala/com/github/opengrabeso/mixtio/mapbox/TileBeltTest.scala
+++ backend/src/test/scala/com/github/opengrabeso/mixtio/mapbox/TileBeltTest.scala
@@ -16,8 +16,8 @@ class TileBeltTest extends AnyFunSuite {
   test("tile to bbox") {
     val ext = tilebelt.tileToBBOX(tile1)
     assert(ext != null)
-    assert(ext.deep == Array(-178.2421875, 84.7060489350415, -177.890625, 84.73838712095339).deep)
-    
+    assert(ext.toSeq == Array(-178.2421875, 84.7060489350415, -177.890625, 84.73838712095339).toSeq)
+
   }
   test("get parent") {
     val parent = tilebelt.getParent(tile1)
@@ -25,13 +25,13 @@ class TileBeltTest extends AnyFunSuite {
     assert(parent(0) == 2)
     assert(parent(1) == 5)
     assert(parent(2) == 9)
-    
+
   }
   test("get siblings") {
     val siblings = tilebelt.getSiblings(tile1)
     assert(siblings.length ==  4)
     assert(siblings(0).length == 3)
-    
+
   }
   test("has siblings") {
     val tiles1 = Array(Array(0, 0, 5), Array(0, 1, 5), Array(1, 1, 5), Array(1, 0, 5))
@@ -40,30 +40,30 @@ class TileBeltTest extends AnyFunSuite {
     assert(tilebelt.hasSiblings(Array(0, 1, 5), tiles1))
     assert(!tilebelt.hasSiblings(Array(0, 0, 5), tiles2))
     assert(!tilebelt.hasSiblings(Array(0, 0, 5), tiles2))
-    
+
   }
   test("has tile") {
     val tiles1 = Array(Array(0, 0, 5), Array(0, 1, 5), Array(1, 1, 5), Array(1, 0, 5))
     assert(!tilebelt.hasSiblings(Array(2, 0, 5), tiles1))
     assert(tilebelt.hasSiblings(Array(0, 1, 5), tiles1))
-    
+
   }
   test("get quadkey") {
     val key = tilebelt.tileToQuadkey(Array(11, 3, 8))
     assert(key == "00001033")
-    
+
   }
   test("quadkey to tile") {
     val quadkey = "00001033"
     val tile = tilebelt.quadkeyToTile(quadkey)
     assert(tile.length == 3)
-    
+
   }
   test("point to tile") {
     val tile = tilebelt.pointToTile(0, 0, 10)
     assert(tile.length == 3)
     assert(tile(2) == 10)
-    
+
   }
   test("point to tile verified") {
     val tile = tilebelt.pointToTile(-77.03239381313323, 38.91326516559442, 10)
@@ -72,17 +72,17 @@ class TileBeltTest extends AnyFunSuite {
     assert(tile(1) == 391)
     assert(tile(2) ==  10)
     assert(tilebelt.tileToQuadkey(tile) == "0320100322")
-    
+
   }
   test("point and tile back and forth") {
     val tile = tilebelt.pointToTile(10, 10, 10)
-    assert(tile.deep == tilebelt.quadkeyToTile(tilebelt.tileToQuadkey(tile)).deep)
-    
+    assert(tile.toSeq == tilebelt.quadkeyToTile(tilebelt.tileToQuadkey(tile)).toSeq)
+
   }
   test("check key 03") {
     val quadkey = "03"
-    assert(tilebelt.quadkeyToTile(quadkey).deep == Array(1, 1, 2).deep)
-    
+    assert(tilebelt.quadkeyToTile(quadkey).toSeq == Array(1, 1, 2).toSeq)
+
   }
   test("bbox to tile -- big") {
     val bbox = Array(-84.72656249999999, 11.178401873711785, -5.625, 61.60639637138628)
@@ -90,13 +90,13 @@ class TileBeltTest extends AnyFunSuite {
     assert(tile(0) == 1)
     assert(tile(1) == 1)
     assert(tile(2) == 2)
-    
+
   }
   test("bbox to tile -- no area") {
     val bbox = Array[Double](-84, 11, -84, 11)
     val tile = tilebelt.bboxToTile(bbox)
-    assert(tile.deep ==  Array(71582788, 125964677, 28).deep)
-    
+    assert(tile.toSeq ==  Array(71582788, 125964677, 28).toSeq)
+
   }
   test("bbox to tile -- dc") {
     val bbox = Array(-77.04615354537964, 38.899967510782346, -77.03664779663086, 38.90728142481329)
@@ -104,7 +104,7 @@ class TileBeltTest extends AnyFunSuite {
     assert(tile(0) == 9371)
     assert(tile(1) == 12534)
     assert(tile(2) == 15)
-    
+
   }
   test("bbox to tile -- crossing 0 lat/lng") {
     val bbox = Array[Double](-10, -10, 10, 10)
@@ -112,7 +112,7 @@ class TileBeltTest extends AnyFunSuite {
     assert(tile(0) == 0)
     assert(tile(1) == 0)
     assert(tile(2) == 0)
-    
+
   }
   test("tile to bbox -- verify bbox order") {
     var tile = Array(13, 11, 5)
@@ -131,14 +131,14 @@ class TileBeltTest extends AnyFunSuite {
     bbox = tilebelt.tileToBBOX(tile)
     assert(bbox(0) < bbox(2), "east is less than west")
     assert(bbox(1) < bbox(3), "south is less than north")
-    
+
   }
   test("pointToTileFraction") {
     val tile = tilebelt.pointToTileFraction(-95.93965530395508, 41.26000108568697, 9)
     assert(tile(0) == 119.552490234375)
     assert(tile(1) == 191.47119140625)
     assert(tile(2) == 9)
-    
+
   }
 
 }
